9c7b8f75bf67c4a32199c38cf5b395f65c857047
hyeonwoojo <h_jo@ejn.gg>
2022-04-25 23:31:50 +0900

    이전 상태와 다를 때 게시물 중복 등록 방어

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ if __name__ == '__main__':
 	for streamer in streamer_list:
 		init_list = asyncio.run(fetch_twitch.fetch(streamer))
 		db_process = db.db_process(database, init_list)
-		if db_process:
+		if db_process:  # 이전 상태와 다를 때 게시글 중복 등록 방어를 위해 추가 요청
+			time.sleep(6)
+			init_list2 = asyncio.run(fetch_twitch.fetch(streamer))
+			db_process2 = db.db_process(database, init_list2)
+			if db_process[1] != db_process2[1]:
+				continue
 			db.write(database, db_process)
 			if db_process[1] == 'ON':  # 뱅온일 때만 등록
 				[subject, content] = write.create_content(database, db_process)
